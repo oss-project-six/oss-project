4eee52cc2174903cdc0d08dd07bb3d16dadabfd9
top6543top <top6543top@gamil.com>
2025-05-25 12:51:29 +0900

    Upload existing project

---
+++ src/main/java/com/example/oss_project/domain/entity/User.java
@@ -0,0 +1,14 @@
+package com.example.oss_project.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "")
+public class User {
+
+
+}
