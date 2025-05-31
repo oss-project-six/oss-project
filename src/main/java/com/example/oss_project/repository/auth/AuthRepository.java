77e1da2343ad9c9a8e0503b005c13b53a33ebf49
JunSeo <dragonpark1004@naver.com>
2025-06-01 04:37:15 +0900

    fix]-#23-dto 수정

--- src/main/java/com/example/oss_project/repository/auth/AuthRepository.java
+++ src/main/java/com/example/oss_project/repository/auth/AuthRepository.java
@@ -1,26 +1,26 @@
-//package com.example.oss_project.repository.auth;
-//
-//import com.example.oss_project.core.exception.CustomException;
-//import com.example.oss_project.core.exception.ErrorCode;
-//import com.example.oss_project.domain.entity.Admin;
-//import com.example.oss_project.domain.entity.User;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//@RequiredArgsConstructor
-//public class AuthRepository {
-//    private final AuthUserJpaRepository authUserJpaRepository;
-//    private final AuthAdminJpaRepository authAdminJpaRepository;
-//
-//    public User findByUserLoginId(String userLoginId){
-//        return authUserJpaRepository.findByLoginId(userLoginId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
-//    }
-//
-//    public Admin findByAdminLoginId(String loginId){
-//        return authAdminJpaRepository.findByLoginId(loginId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ADMIN));
-//    }
-//
-//    public User save(User user){ return authUserJpaRepository.save(user); }
-//    public Admin save(Admin admin){ return authAdminJpaRepository.save(admin); }
-//}
+package com.example.oss_project.repository.auth;
+
+import com.example.oss_project.core.exception.CustomException;
+import com.example.oss_project.core.exception.ErrorCode;
+import com.example.oss_project.domain.entity.Admin;
+import com.example.oss_project.domain.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class AuthRepository {
+    private final AuthUserJpaRepository authUserJpaRepository;
+    private final AuthAdminJpaRepository authAdminJpaRepository;
+
+    public User findByUserLoginId(String userLoginId){
+        return authUserJpaRepository.findByLoginId(userLoginId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+    }
+
+    public Admin findByAdminLoginId(String loginId){
+        return authAdminJpaRepository.findByLoginId(loginId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ADMIN));
+    }
+
+    public User save(User user){ return authUserJpaRepository.save(user); }
+    public Admin save(Admin admin){ return authAdminJpaRepository.save(admin); }
+}
--- src/main/java/com/example/oss_project/service/auth/SignUpService.java
+++ src/main/java/com/example/oss_project/service/auth/SignUpService.java
@@ -1,45 +1,45 @@
-//package com.example.oss_project.service.auth;
-//
-//import com.example.oss_project.core.security.SecurityConfig;
-//import com.example.oss_project.domain.entity.Admin;
-//import com.example.oss_project.domain.entity.User;
-//import com.example.oss_project.domain.request.auth.SignUpRequestDto;
-//import com.example.oss_project.domain.type.AuthStatus;
-//import com.example.oss_project.repository.auth.AuthRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@Transactional
-//@RequiredArgsConstructor
-//@Service
-//public class SignUpService {
-//    private final AuthRepository authRepository;
-//    private final PasswordEncoder passwordEncoder;
-//
-//    public Boolean signUp(SignUpRequestDto signUpRequestDto){
-//
-//        if(!signUpRequestDto.password().equals(signUpRequestDto.rePassword())){
-//            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-//        }
-//        if(signUpRequestDto.authStatus() == AuthStatus.USER){
-//            User user = User.builder()
-//                    .userLoginId(signUpRequestDto.id())
-//                    .nickname(signUpRequestDto.nickname())
-//                    .password(passwordEncoder.encode(signUpRequestDto.password()))
-//                    .build();
-//
-//            authRepository.save(user);
-//        } else {
-//            Admin admin = Admin.builder()
-//                    .adminLoginId(signUpRequestDto.id())
-//                    .nickname(signUpRequestDto.nickname())
-//                    .password(passwordEncoder.encode(signUpRequestDto.password()))
-//                    .build();
-//            authRepository.save(admin);
-//        }
-//
-//        return true;
-//    }
-//}
+package com.example.oss_project.service.auth;
+
+import com.example.oss_project.core.security.SecurityConfig;
+import com.example.oss_project.domain.entity.Admin;
+import com.example.oss_project.domain.entity.User;
+import com.example.oss_project.domain.request.auth.SignUpRequestDto;
+import com.example.oss_project.domain.type.AuthStatus;
+import com.example.oss_project.repository.auth.AuthRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class SignUpService {
+    private final AuthRepository authRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Boolean signUp(SignUpRequestDto signUpRequestDto){
+
+        if(!signUpRequestDto.password().equals(signUpRequestDto.rePassword())){
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+        if(signUpRequestDto.authStatus() == AuthStatus.USER){
+            User user = User.builder()
+                    .userLoginId(signUpRequestDto.id())
+                    .nickname(signUpRequestDto.nickname())
+                    .password(passwordEncoder.encode(signUpRequestDto.password()))
+                    .build();
+
+            authRepository.save(user);
+        } else {
+            Admin admin = Admin.builder()
+                    .adminLoginId(signUpRequestDto.id())
+                    .nickname(signUpRequestDto.nickname())
+                    .password(passwordEncoder.encode(signUpRequestDto.password()))
+                    .build();
+            authRepository.save(admin);
+        }
+
+        return true;
+    }
+}
